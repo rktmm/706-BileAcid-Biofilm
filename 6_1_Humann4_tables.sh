6790094b6d44971d33c4070dd2f5be5b56ff992c
rktmm <36552592+rktmm@users.noreply.github.com>
2026-06-18 13:44:07 +0100

    Removal of details and upload conda envs
    Tidy up for usability
    
    Upload conda env and additional notes and updates to README
    
    env.yml files
    
    README for envs and db used
    additional notes update when I first booted up the VM and disks

--- 6_1_Humann4_tables.sh
+++ 6_1_Humann4_tables.sh
@@ -4,29 +4,38 @@
 
 date -u
 
+# Usage: bash $0 <project_ID_out_bucket>
+
+project_id_out_bucket="$1"
+
+if [[ -z "$project_id" ]]; then 
+    echo " Usage: bash $0 <project_ID_out_bucket>"
+    exit 1
+fi
+
 # Load modules
 source /opt/miniforge3/etc/profile.d/conda.sh
 conda activate humann4a
-export HUMANN_CONFIG_FOLDER=/mnt/data-disk/tmp_scratch/database/humann4_db
+export HUMANN_CONFIG_FOLDER=/mnt/data-disk/tmp_scratch/database/humann4_db # this is the specific location of db used
 humann_config --print
 
 # for this particular analysis - it has been run in batches - therefore collate all tsv files into one folder
 # file locations
-#   ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis/humann4_out-batch-00*/
+#   ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis/humann4_out-batch-00*/
 #   containing and needisolating and combining the following files:
 #   *_1_metaphlan_profile.tsv
 #   *_2_genefamilies.tsv
 #   *_3_reactions.tsv
 #   *_4_pathabundance.tsv
 
-cd ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis
+cd ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis
 mkdir -p hmn4_collated_out
-find ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis \
+find ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis \
 -type f \( -name "*genefamilies.tsv" -o \
 -name "*reactions.tsv" -o \
 -name "*pathabundance.tsv" \) \
 ! -path "*/hmn4_collated_out/*" \
--exec cp -n -v -t ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis/hmn4_collated_out/ {} +
+-exec cp -n -v -t ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis/hmn4_collated_out/ {} +
 # all files in /hmn4_collated_out
 
 # combine sample tables for gene families, reactions, and pathway abundance - step 1
--- 8_1_extra.sh
+++ 8_1_extra.sh
@@ -3,23 +3,32 @@
 # ad hoc additions
 date -u
 
+# Usage: bash $0 <project_ID_out_bucket>
+
+project_id_out_bucket="$1"
+
+if [[ -z "$project_id" ]]; then 
+    echo " Usage: bash $0 <project_ID_out_bucket>"
+    exit 1
+fi
+
 # Load modules
 source /opt/miniforge3/etc/profile.d/conda.sh
 conda activate humann4a
 
 # split strat regrouped and renormed (but not renamed) genefamilies
-cd ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis
+cd ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis
 echo "######## Running split stratification of tables"
 humann_split_stratified_table --input hmn4_genefamilies_regrp_renorm_cpm.tsv \
 --output hmn4_genefamilies_regrp_renorm_cpm_stratification_out
 
 # metaphlan4 combine profiles
-cd ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis
+cd ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis
 mkdir -p metaphlan_profiles_collated_out
-find ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis \
+find ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis \
 -type f -name "*metaphlan_profile.tsv" \
 ! -path "*/metaphlan_profiles_collated_out/*" \
--exec cp -n -v -t ~/gcsfuse/mhra-ngs-dev-b9su_output/evette-humann-analysis/metaphlan_profiles_collated_out/ {} +
+-exec cp -n -v -t ~/gcsfuse/"$project_id_out_bucket"/evette-humann-analysis/metaphlan_profiles_collated_out/ {} +
 
 merge_metaphlan_tables.py metaphlan_profiles_collated_out/*_profile.tsv > merged_abundance_table.tsv
 
